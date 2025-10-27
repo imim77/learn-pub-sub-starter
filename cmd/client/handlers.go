fe9de5c5cec3247e178118e254722f4e7faffe4d
mvusic07 <thinkman405@gmail.com>
2025-10-27 15:51:57 +0100

    1.

---
+++ cmd/client/handlers.go
@@ -0,0 +1,9 @@
+package main
+
+/*func handlerPause(gs *gamelogic.GameState) func(routing.PlayingState) {
+	return func(ps routing.PlayingState) {
+		defer fmt.Print("> ")
+		gs.HandlePause(ps)
+	}
+}
+*/
